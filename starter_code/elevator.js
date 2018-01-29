aebd73205954755bf1558e3c18b5987ad0efb03f
Kathia Rieser <kath.medulla@gmail.com>
2018-01-29 19:04:44 +0100

    done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,59 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.waitingList = [];
+    this.passengers = [];
+    this.directionUp = true; // up or down
+    this.intervalId;
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+
+      this.intervalId = setInterval(() => this.update(), 1000);
+
+   }
+  stop() {
+
+      clearInterval(this.intervalId);
+
+   }
+  update() { 
+
+    this.log();
+    this.floorUp();
+    this.floorDown();
+    setTimeout(() => this.stop(), 30000);
+  }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  floorUp() { 
+      if(this.directionUp && this.floor < this.MAXFLOOR){
+        this.floor++ ;
+      }else{
+        this.directionUp = false;
+      }
+      
+  }
+  floorDown() { 
+
+      if(!this.directionUp && this.floor > 0){
+        this.floor--;
+      }else{
+        this.directionUp = true;
+      }
+      
+  }
+  call(person) { 
+
+    this.requests.push(person);
+    this.waitingList.push(person);
+  }
+  log() {
+
+    let direction = this.directionUp? "Up" :"Down";
+    console.log("Direction: "+  direction + " | Floor: "+ this.floor );
+
+   }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,14 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+var elevator = new Elevator();
+
+elevator.start();
+
+var personProof = new Person("Kathia", 3, 7);
+elevator.call(personProof);
+console.log(elevator.requests);
+console.log(elevator.waitingList);
+
+
